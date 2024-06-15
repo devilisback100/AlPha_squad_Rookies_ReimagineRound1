638b81eba0df1adbe8eb4d0eef6d10677c25b11d
suresh paliwal <112789553+devilisback100@users.noreply.github.com>
2024-06-15 13:10:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           <Slider />
           <BestSelletSection />
           <SummerSellSection />
-          // <Blogs />
+          
           <BottomSection />
           <Footer />
         </>
